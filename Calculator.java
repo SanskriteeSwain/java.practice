2373c766eda4c02a5a54d6a009685e58c8584912
SanskriteeSwain <sanskriteeswain@gmail.com>
2026-09-18 08:49:17 +0530

    Added Smart Calculator project

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,8 @@ public class Calculator {
   public static void main(String[] args)  {
 
      Scanner input = new Scanner(System.in);
+
+    System.out.println("===SMART CALCULATOR===");
      
   System.out.print("Enter first number: ");
       double num1 = input.nextDouble();
