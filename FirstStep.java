2f0d6dc8d1f29bb04cbedec91380a2165ff0276c
SanskriteeSwain <sanskriteeswain@gmail.com>
2026-09-18 06:58:27 +0530

    Added my first java project

---
+++ FirstStep.java
@@ -0,0 +1,6 @@
+public class FirstStep {
+       public static void main(String[] args) {
+        System.out.println("My first step into java");
+        System.out.println("GitHub journey begins!");
+         }
+      } 
